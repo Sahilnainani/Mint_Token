c7c8a385237d43f0ff646b389f5bbf07a3db6bc9
Sahil Nainani <sahilnainani45226@gmail.com>
2023-04-18 23:21:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Abi from './artifacts/contracts/Lock.sol/Lock.json'
 import { useState } from 'react';
 
 function App() {
+
+  const [state,setState] = useState({
+    result_name : null,
+    result_symbol : null,
+    result_sypply : null,
+    result_decimals : null
+  })
+
   const [name, setName] = useState("");
   const [symbol, setSymbol] = useState("");
   const [decimals, setDecimals] = useState(18);
@@ -26,7 +34,7 @@ function App() {
       const result_symbol = await currentContract.symbol();
       const result_sypply = await currentContract.totalSupply();
       const result_decimals = await currentContract.decimals();
-      
+      setState({result_name,result_symbol,result_sypply,result_decimals});
       console.log(`      Name of Token:${result_name},
       Symbol of Token:${result_symbol},
       TotalSupply of Token:${result_sypply}
@@ -68,8 +76,16 @@ function App() {
       </label> */}
       <br />
       <button type="button" onClick={mintTokens}>Mint Tokens</button>
-      <div>{status}</div>
+      <div style={{marginTop:"40px", display: "flex", justifyContent: "center", alignItems:"center"}}>{status}</div>
       </form>
+      {
+        state.result_name ? (<div style={{marginTop:"40px", display: "flex", justifyContent: "center", alignItems:"center"}}>
+          <p style={{border:"solid black 5px",padding:"8px",marginLeft:"20px"}}>Name : {String(state.result_name)}</p>
+          <p style={{border:"solid black 5px",padding:"8px",marginLeft:"20px"}}>Symbol : {String(state.result_symbol)}</p>
+          <p style={{border:"solid black 5px",padding:"8px",marginLeft:"20px"}}>Decimal : {String(state.result_decimals)}</p>
+          <p style={{border:"solid black 5px",padding:"8px",marginLeft:"20px"}}>Initial Supply : {String(state.result_sypply)}</p>
+        </div>) : (<div></div>)
+      }
     </div>
   );
 }
